a1995bc36f44b7099f24c71352898d122dd661e9
jake whiteley <odegramusic@gmail.com>
2018-05-20 21:53:40 +0100

    bootsrap the class

--- src/Service_Provider.php
+++ src/Service_Provider.php
@@ -2,7 +2,30 @@
 
 namespace Snap\Whoops;
 
-class Service_Provider
+use Snap\Core\Service_Provider;
+
+class Whoops_Service_Provider extends Service_Provider
 {
+	public $services = [];
 	
+	public $factories = [];
+
+	/**
+	 * Called after all service providers have been registered.
+	 *
+	 * @since 1.0.0
+	 */
+	public function boot()
+	{
+		// hooks/filters? call the whoops bootstrap
+	}
+
+	/**
+	 * Register any
+	 * @return [type] [description]
+	 */
+	public function register()
+	{
+
+	}
 }
